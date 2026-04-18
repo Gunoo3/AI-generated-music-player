da8ae5b91ce77ac1e2d7150912e262e02e4818a6
Gunoo3 <knthong@yahoo.com>
2026-04-18 15:22:31 -0400

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("playlistAPI", {
-  save: data => ipcRenderer.send("save-playlist", data),
-  load: () => ipcRenderer.invoke("load-playlist")
-});
